795d43c70498ee3a777d0f08d9ecc22ee03d7a8b
kalal Chandrashekar <kalalchandrashekar9963@gmail.com>
2021-05-20 16:31:29 +0530

    Initial commit

---
+++ src/com/example/operators/EqualAndNotEqual.java
@@ -0,0 +1,12 @@
+package com.example.operators;
+
+public class EqualAndNotEqual {
+    public static void main(String[] args) {
+        int x,y;
+        x = 10;
+        y = 15;
+        System.out.println(x == y);
+        System.out.println(x != y);
+
+    }
+}
